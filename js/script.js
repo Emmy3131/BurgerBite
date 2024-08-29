511c419cfe6b985a34781839cdbc364422d54bb6
Emmy3131 <devinechild30032@gmail.com>
2024-08-29 10:49:40 +0100

    WIP: am done with the design view

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const heroSwiper = new Swiper('.heroSwiper', {
     // Optional parameters
     loop: true,
   
@@ -10,4 +10,23 @@ const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 4000
     }
+  });
+
+  const burgerSwiper = new Swiper('.burgerSwiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween:40,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+        delay: 4000
+    }
+
+    // If we need navigation buttons
+    
   });
